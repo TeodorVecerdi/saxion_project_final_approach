d58d86afe05a8fadeb0b40729b34badc98cd7292
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2020-05-05 17:27:50 +0200

    Implement basic server for project networking

--- server.js
+++ server.js
@@ -1,38 +1,98 @@
-const io = require("socket.io");
-const server = io.listen(8000);
+const io = require('socket.io');
+let port = process.env.PORT || 8080;
+const server = io.listen(port);
 
-let sequenceNumberByClient = new Map();
-let clientPositions = new Map();
+let players = {};
+let onlinePlayers = {};
 
-// event fired every time a new client connects:
+let socketToPlayerId = {};
+let socketIdToSocket = {};
+let playerIdToSocket = {};
+
+// TODO: HANDLE REQUESTS /////////////
+// online_players_request ////////////
+// TODO: IMPLEMENT ACCOUNT CREATION //
+
 server.on("connection", (socket) => {
     console.info(`Client connected [id=${socket.id}]`);
-    socket.emit('gimme-position');
-    clientPositions.set(socket, {x: 0.0, y: 0.0});
 
-    // initialize this client's sequence number
-    sequenceNumberByClient.set(socket, 1);
+    socket.on('data_request', data => {
+        if (players.hasOwnProperty(data)) {
+            onlinePlayers[data] = true;
+            socketToPlayerId[socket] = data;
+            socketIdToSocket[socket.id] = socket;
+            playerIdToSocket[data] = socket;
 
-    socket.on("hi", () => {
-        console.log(`Client[id=${socket.id}] said hi! :)`)
-        socket.emit("hi", "HELLO CLIENT!");
-    })
+            socket.emit('data_request_success', players[data]);
+            broadcast(socket, 'client_initialised', players[data]);
+        } else {
+            socket.emit('data_request_fail');
+        }
+    });
+    socket.on('initialised', data => {
+        let playerData = JSON.parse(data);
 
-    // when socket disconnects, remove it from the list:
+        onlinePlayers[playerData["id"]] = true;
+        socketToPlayerId[socket] = playerData["id"];
+        socketIdToSocket[socket.id] = socket;
+        playerIdToSocket[playerData["id"]] = socket;
+
+        //TODO: Add more fields here --//
+        players[playerData["id"]] = {
+            id: playerData["id"],
+            name: playerData["name"],
+            position: playerData["position"]
+        };
+        //TODO: -----------------------//
+        broadcast(socket, 'client_initialised', players[playerData["id"]]);
+    });
+
     socket.on("disconnect", () => {
-        sequenceNumberByClient.delete(socket);
+        onlinePlayers[socketToPlayerId[socket]] = false;
+        delete socketToPlayerId[socket];
+        delete socketIdToSocket[socket.id];
+        delete playerIdToSocket[socketToPlayerId[socket]];
+
+        broadcast(socket, 'client_disconnected', {'playerId': socketToPlayerId[socket]});
         console.info(`Client gone [id=${socket.id}]`);
     });
 
-    socket.on('update-position', (data) => {
+    socket.on('updated', (data) => {
+        let playerData = JSON.parse(data);
 
-        console.log("new position incoming");
-        var obj = JSON.parse(data);
-        clientPositions.set(socket, {x: obj.position.x, y: obj.position.y});
-        console.log(clientPositions.get(socket));
-        socket.broadcast.emit("new-position", { socket: socket, pos: clientPositions.get(socket)});
+        //TODO: Add more fields here --//
+        players[socketToPlayerId[socket]]["position"] = playerData["position"];
+        //TODO: -----------------------//
 
-        // socket.broad
+        broadcast(socket, 'client_updated', players[socketToPlayerId[socket]]);
     });
-    
+
+    socket.on('debug', (data) => {
+        console.log(getDebugData());
+        socket.emit('debug', getDebugData())
+    });
 });
+
+let getDebugData = () => {
+    return {
+        'players': players,
+        'onlinePlayers': onlinePlayers
+    };
+};
+
+let broadcast = (socket, message, data) => {
+    for (let key in onlinePlayers) {
+        if (onlinePlayers.hasOwnProperty(key)) {
+            let targetSocket = playerIdToSocket[key];
+            let value = onlinePlayers[key];
+            if (value === true && targetSocket.id !== socket.id) {
+                targetSocket.emit(message, data);
+                console.info(`broadcasted ${message} to [pID,sID] : [${key},${targetSocket.id}]`);
+            }
+        }
+    }
+};
+/*setInterval(() => {
+    console.log(getDebugData());
+}, 5000);*/
+console.log(`Server started on port ${port}`);
