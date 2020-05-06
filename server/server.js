2246fa9c1cd9af32cc9a5777967e66083cbad9f2
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2020-05-06 17:36:32 +0200

    Finish server part of networking

--- server/server.js
+++ server/server.js
@@ -5,23 +5,23 @@ const server = io.listen(port);
 let players = {};
 let onlinePlayers = {};
 
-let socketToPlayerId = {};
 let socketIdToSocket = {};
-let playerIdToSocket = {};
+let socketIdToPlayerId = {};
+let playerIdToSocketId = {};
 
 // TODO: HANDLE REQUESTS /////////////
 // online_players_request ////////////
 // TODO: IMPLEMENT ACCOUNT CREATION //
 
-server.on("connection", (socket) => {
+server.on("connection", socket => {
     console.info(`Client connected [id=${socket.id}]`);
 
     socket.on('data_request', data => {
         if (players.hasOwnProperty(data)) {
             onlinePlayers[data] = true;
-            socketToPlayerId[socket] = data;
+            socketIdToPlayerId[socket.id] = data;
             socketIdToSocket[socket.id] = socket;
-            playerIdToSocket[data] = socket;
+            playerIdToSocketId[data] = socket.id;
 
             socket.emit('data_request_success', players[data]);
             broadcast(socket, 'client_initialised', players[data]);
@@ -33,9 +33,9 @@ server.on("connection", (socket) => {
         let playerData = JSON.parse(data);
 
         onlinePlayers[playerData["id"]] = true;
-        socketToPlayerId[socket] = playerData["id"];
+        socketIdToPlayerId[socket.id] = playerData["id"];
         socketIdToSocket[socket.id] = socket;
-        playerIdToSocket[playerData["id"]] = socket;
+        playerIdToSocketId[playerData["id"]] = socket.id;
 
         //TODO: Add more fields here --//
         players[playerData["id"]] = {
@@ -47,27 +47,47 @@ server.on("connection", (socket) => {
         broadcast(socket, 'client_initialised', players[playerData["id"]]);
     });
 
-    socket.on("disconnect", () => {
-        onlinePlayers[socketToPlayerId[socket]] = false;
-        delete socketToPlayerId[socket];
-        delete socketIdToSocket[socket.id];
-        delete playerIdToSocket[socketToPlayerId[socket]];
-
-        broadcast(socket, 'client_disconnected', {'playerId': socketToPlayerId[socket]});
+    socket.on("disconnect", data => {
         console.info(`Client gone [id=${socket.id}]`);
+        broadcast(socket, 'client_disconnected', {'playerId': socketIdToPlayerId[socket.id]});
+
+        let socketId = socket.id;
+        let playerId = socketIdToPlayerId[socketId];
+
+        onlinePlayers[playerId] = false;
+        delete socketIdToPlayerId[socketId];
+        delete socketIdToSocket[socketId];
+        delete playerIdToSocketId[playerId];
     });
 
-    socket.on('updated', (data) => {
+    socket.on('updated', data => {
         let playerData = JSON.parse(data);
+        let playerId = socketIdToPlayerId[socket.id];
+        if (players[playerId] === undefined) return;
 
         //TODO: Add more fields here --//
-        players[socketToPlayerId[socket]]["position"] = playerData["position"];
+        players[playerId]["position"] = playerData["position"];
         //TODO: -----------------------//
 
-        broadcast(socket, 'client_updated', players[socketToPlayerId[socket]]);
+        broadcast(socket, 'client_updated', players[playerId]);
     });
 
-    socket.on('debug', (data) => {
+    socket.on('online_players_request', data => {
+        let playerId = socketIdToPlayerId[socket.id];
+        console.info(`Player with GUID ${playerId} is requesting online players`);
+        let onlinePlayersData = {};
+        for(let key in onlinePlayers) {
+            if(onlinePlayers.hasOwnProperty(key)) {
+                if(onlinePlayers[key] === true && key !== playerId) {
+                    onlinePlayersData[key] = players[key];
+                }
+            }
+        }
+        console.info(`Sending online players: ${JSON.stringify(onlinePlayersData)}`);
+        socket.emit('online_players', onlinePlayersData);
+    });
+
+    socket.on('debug', data => {
         console.log(getDebugData());
         socket.emit('debug', getDebugData())
     });
@@ -76,23 +96,27 @@ server.on("connection", (socket) => {
 let getDebugData = () => {
     return {
         'players': players,
-        'onlinePlayers': onlinePlayers
+        'onlinePlayers': onlinePlayers,
+        's2p': socketIdToPlayerId
     };
 };
 
 let broadcast = (socket, message, data) => {
     for (let key in onlinePlayers) {
         if (onlinePlayers.hasOwnProperty(key)) {
-            let targetSocket = playerIdToSocket[key];
+            let targetSocketId = playerIdToSocketId[key];
+            let targetSocket = socketIdToSocket[targetSocketId];
+            if(targetSocket === undefined) continue;
+
             let value = onlinePlayers[key];
-            if (value === true && targetSocket.id !== socket.id) {
+            if (value === true && (socket === undefined || targetSocketId !== socket.id)) {
                 targetSocket.emit(message, data);
-                console.info(`broadcasted ${message} to [pID,sID] : [${key},${targetSocket.id}]`);
+                // console.info(`broadcasted ${message} to [pID,sID] : [${key},${targetSocket.id}]`);
             }
         }
     }
 };
-/*setInterval(() => {
-    console.log(getDebugData());
-}, 5000);*/
+setInterval(() => {
+    broadcast(undefined, 'update', {});
+}, 25);
 console.log(`Server started on port ${port}`);
