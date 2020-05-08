56232fa77d0e69f008f6518091e0cbd970d31571
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2020-05-08 19:11:46 +0200

    Finish user registration prototype

--- client/Game/App.cs
+++ client/Game/App.cs
@@ -13,8 +13,8 @@ namespace game {
             SetupInput();
 
             var username = "";
-            var avatar = 0;
-            
+            var avatarIndex = 0;
+
             var mainUI = new Pivot();
             AddChild(mainUI);
 
@@ -43,7 +43,7 @@ namespace game {
             #region BODY
             var textFieldLabelStyle = new LabelStyle(Color.FromArgb(205, 205, 205), 14f, FontLoader.LeftCenterAlignment);
             body.AddChild(new Label(0, 0, 500, 24, "Username", textFieldLabelStyle));
-            body.AddChild(new TextField(0, 24, 500, 40, "", new TextFieldStyle(placeholderTextSizeNormal: 12f, placeholderTextSizeFocused: 12f), onValueChanged: (oldValue, newValue) => username=newValue));
+            body.AddChild(new TextField(0, 24, 500, 40, "", new TextFieldStyle(placeholderTextSizeNormal: 12f, placeholderTextSizeFocused: 12f), onValueChanged: (oldValue, newValue) => username = newValue));
             body.AddChild(new Label(0, 84, 500, 24, "Avatar", textFieldLabelStyle));
 
             var avatarButtonStyle = new ButtonStyle(
@@ -51,43 +51,70 @@ namespace game {
                 textNormal: Color.FromArgb(93, 55, 170), textHover: Color.FromArgb(106, 52, 178), textPressed: Color.FromArgb(111, 56, 189),
                 fontLoaderInstance: FontLoader.SourceCodeBold);
             var avatarContainer = new Pivot {scaleX = 0f, x = 160, y = 108};
-            var avatarButton = new SpriteButton(0, 108, 128, 128, "Click\r\nto switch", Texture2D.GetInstance("data/sprites/avatar_blue_sm.png", true), avatarButtonStyle, () => { avatarContainer.scaleX = Math.Abs(avatarContainer.scaleX) < 0.0001f ? 1f : 0f; });
+            var avatarButton = new SpriteButton(0, 108, 128, 128, "Click\r\nto switch", new Sprite("data/sprites/avatar_blue_sm.png", true), avatarButtonStyle, () => { avatarContainer.scaleX = Math.Abs(avatarContainer.scaleX) < 0.0001f ? 1f : 0f; });
             body.AddChild(avatarButton);
 
             body.AddChild(avatarContainer);
             {
                 #region AVATAR_CONTAINER
-                avatarContainer.AddChild(new SpriteButton(0, 0, 128, 128, "", Texture2D.GetInstance("data/sprites/avatar_blue_sm.png", true), avatarButtonStyle, () => {
-                    avatarButton.Texture = Texture2D.GetInstance("data/sprites/avatar_blue_sm.png", true);
+                avatarContainer.AddChild(new SpriteButton(0, 0, 128, 128, "", new Sprite("data/sprites/avatar_blue_sm.png", true), avatarButtonStyle, () => {
+                    avatarButton.Sprite = new Sprite("data/sprites/avatar_blue_sm.png", true);
                     avatarButton.ShouldRepaint = true;
                     avatarContainer.scaleX = 0f;
-                    avatar = 0;
+                    avatarIndex = 0;
                 }));
-                avatarContainer.AddChild(new SpriteButton(138, 0, 128, 128, "", Texture2D.GetInstance("data/sprites/avatar_green_sm.png", true), avatarButtonStyle, () => {
-                    avatarButton.Texture = Texture2D.GetInstance("data/sprites/avatar_green_sm.png", true);
+                avatarContainer.AddChild(new SpriteButton(138, 0, 128, 128, "", new Sprite("data/sprites/avatar_green_sm.png", true), avatarButtonStyle, () => {
+                    avatarButton.Sprite = new Sprite("data/sprites/avatar_green_sm.png", true);
                     avatarButton.ShouldRepaint = true;
                     avatarContainer.scaleX = 0f;
-                    avatar = 1;
+                    avatarIndex = 1;
                 }));
-                avatarContainer.AddChild(new SpriteButton(276, 0, 128, 128, "", Texture2D.GetInstance("data/sprites/avatar_red_sm.png", true), avatarButtonStyle, () => {
-                    avatarButton.Texture = Texture2D.GetInstance("data/sprites/avatar_red_sm.png", true);
+                avatarContainer.AddChild(new SpriteButton(276, 0, 128, 128, "", new Sprite("data/sprites/avatar_red_sm.png", true), avatarButtonStyle, () => {
+                    avatarButton.Sprite = new Sprite("data/sprites/avatar_red_sm.png", true);
                     avatarButton.ShouldRepaint = true;
                     avatarContainer.scaleX = 0f;
-                    avatar = 2;
+                    avatarIndex = 2;
                 }));
-                avatarContainer.AddChild(new SpriteButton(414, 0, 128, 128, "", Texture2D.GetInstance("data/sprites/avatar_yellow_sm.png", true), avatarButtonStyle, () => {
-                    avatarButton.Texture = Texture2D.GetInstance("data/sprites/avatar_yellow_sm.png", true);
+                avatarContainer.AddChild(new SpriteButton(414, 0, 128, 128, "", new Sprite("data/sprites/avatar_yellow_sm.png", true), avatarButtonStyle, () => {
+                    avatarButton.Sprite = new Sprite("data/sprites/avatar_yellow_sm.png", true);
                     avatarButton.ShouldRepaint = true;
                     avatarContainer.scaleX = 0f;
-                    avatar = 3;
+                    avatarIndex = 3;
                 }));
                 #endregion
             }
             body.AddChild(new Button(20, Globals.HEIGHT - 40 - 190 - 30, Globals.WIDTH - 80, 40, "Submit", onClick: () => {
-                
+                if (string.IsNullOrEmpty(username)) return;
+                NetworkManager.Instance.Initialize(username, avatarIndex);
+                mainUI.Destroy();
+                CreateTempLoggedInUI();
             }));
             #endregion
             #endregion
+        }
+
+        private void CreateTempLoggedInUI() {
+            var temp = new Pivot();
+            AddChild(temp);
+            
+            var primaryTitleStyle = new LabelStyle(Color.White, 32f, FontLoader.CenterCenterAlignment);
+            var secondaryTitleStyle = new LabelStyle(Color.White, 20f, FontLoader.LeftCenterAlignment);
+            var secondaryTitleStyleBold = new LabelStyle(Color.White, 20f, FontLoader.LeftCenterAlignment, FontLoader.SourceCodeBold);
+            temp.AddChild(new Label(10, 10, Globals.WIDTH, 60, "Successfully logged in!", primaryTitleStyle));
+            temp.AddChild(new Label(40, 100, Globals.WIDTH, 40, $"Username:", secondaryTitleStyle));
+            temp.AddChild(new Label(370, 100, Globals.WIDTH, 40, $"{NetworkManager.Instance.Username}", secondaryTitleStyleBold));
+            temp.AddChild(new Label(40, 150, Globals.WIDTH, 40, $"Unique Identifier:", secondaryTitleStyle));
+            temp.AddChild(new Label(370, 150, Globals.WIDTH, 40, $"{NetworkManager.Instance.GUID}", secondaryTitleStyleBold));
+            temp.AddChild(new Label(40, 200, Globals.WIDTH, 40, $"Room Id:", secondaryTitleStyle));
+            temp.AddChild(new Label(370, 200, Globals.WIDTH, 40, $"{NetworkManager.Instance.RoomID}", secondaryTitleStyleBold));
+            temp.AddChild(new Label(40, 250, Globals.WIDTH, 40, $"Avatar:", secondaryTitleStyle));
+            Sprite texture;
+            if(NetworkManager.Instance.AvatarIndex == 0) texture = new Sprite("data/sprites/avatar_blue.png");
+            else if(NetworkManager.Instance.AvatarIndex == 1) texture = new Sprite("data/sprites/avatar_green.png");
+            else if (NetworkManager.Instance.AvatarIndex == 2) texture = new Sprite("data/sprites/avatar_red.png");
+            else texture = new Sprite("data/sprites/avatar_yellow.png");
+            texture.SetScaleXY(0.5f,0.5f);
+            temp.AddChild(new SpriteButton(370, 250, 512, 512, "", texture));
         }
 
         private void SetupInput() {
