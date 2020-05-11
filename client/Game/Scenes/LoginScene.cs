cbb2becb648f7566abe6698297801c850635730e
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2020-05-11 13:22:51 +0200

    Change avatar pictures on login screen

--- client/Game/Scenes/LoginScene.cs
+++ client/Game/Scenes/LoginScene.cs
@@ -46,36 +46,43 @@ namespace game {
             body.AddChild(new TextField(0, 24, 500, 40, "", new TextFieldStyle(placeholderTextSizeNormal: 12f, placeholderTextSizeFocused: 12f), onValueChanged: (oldValue, newValue) => username = newValue));
             body.AddChild(new Label(0, 84, 500, 24, "Avatar", textFieldLabelStyle));
             var avatarContainer = new Pivot {x = -100000f, y = 108};
-            var avatarButton = new SpriteButton(0, 108, 128, 128, "Click\r\nto switch", new Sprite("data/sprites/avatar_blue_sm.png", true), avatarButtonStyle, () => {
+            var avatarButton = new SpriteButton(0, 108, 128, 128, "Click\r\nto switch", new Sprite("data/sprites/avatars/female_1_128.png", true), avatarButtonStyle, () => {
                 avatarContainer.x = Math.Abs(avatarContainer.x - 160f) < 0.0001f ? -100000f : 160f;
             });
             body.AddChild(avatarButton);
             body.AddChild(avatarContainer);
-            avatarContainer.AddChild(new SpriteButton(0, 0, 128, 128, "", new Sprite("data/sprites/avatar_blue_sm.png", true), avatarButtonStyle, () => {
-                avatarButton.Sprite = new Sprite("data/sprites/avatar_blue_sm.png", true);
-                avatarButton.ShouldRepaint = true;
-                avatarContainer.x = -100000f;
-                avatarIndex = 0;
-            }));
-            avatarContainer.AddChild(new SpriteButton(138, 0, 128, 128, "", new Sprite("data/sprites/avatar_green_sm.png", true), avatarButtonStyle, () => {
-                avatarButton.Sprite = new Sprite("data/sprites/avatar_green_sm.png", true);
-                avatarButton.ShouldRepaint = true;
-                avatarContainer.x = -100000f;
-                avatarIndex = 1;
-            }));
-            avatarContainer.AddChild(new SpriteButton(276, 0, 128, 128, "", new Sprite("data/sprites/avatar_red_sm.png", true), avatarButtonStyle, () => {
-                avatarButton.Sprite = new Sprite("data/sprites/avatar_red_sm.png", true);
-                avatarButton.ShouldRepaint = true;
-                avatarContainer.x = -100000f;
-                avatarIndex = 2;
-            }));
-            avatarContainer.AddChild(new SpriteButton(414, 0, 128, 128, "", new Sprite("data/sprites/avatar_yellow_sm.png", true), avatarButtonStyle, () => {
-                avatarButton.Sprite = new Sprite("data/sprites/avatar_yellow_sm.png", true);
-                avatarButton.ShouldRepaint = true;
-                avatarContainer.x = -100000f;
-                avatarIndex = 3;
-            }));
-            body.AddChild(new Checkbox(0, 270, Globals.WIDTH - 40, 70, "Would you like to share anonymous\nusage statistics to help us\nimprove the user experience?", onValueChanged: (oldValue, newValue) => {
+            var female1 = UIFactory.CreateAvatarSelectionEntry(128 * 0, 0, "data/sprites/avatars/female_1_128.png", avatarButton, avatarContainer, avatarButtonStyle);
+            female1.OnClick += () => avatarIndex = 0;
+            var female2 = UIFactory.CreateAvatarSelectionEntry(128 * 1 + 10, 0, "data/sprites/avatars/female_2_128.png", avatarButton, avatarContainer, avatarButtonStyle);
+            female2.OnClick += () => avatarIndex = 1;
+            var female3 = UIFactory.CreateAvatarSelectionEntry(128 * 2 + 10, 0, "data/sprites/avatars/female_3_128.png", avatarButton, avatarContainer, avatarButtonStyle);
+            female3.OnClick += () => avatarIndex = 2;
+            var female4 = UIFactory.CreateAvatarSelectionEntry(128 * 3 + 10, 0, "data/sprites/avatars/female_4_128.png", avatarButton, avatarContainer, avatarButtonStyle);
+            female4.OnClick += () => avatarIndex = 3;
+            var female5 = UIFactory.CreateAvatarSelectionEntry(128 * 4 + 10, 0, "data/sprites/avatars/female_5_128.png", avatarButton, avatarContainer, avatarButtonStyle);
+            female5.OnClick += () => avatarIndex = 4;
+            var male1 = UIFactory.CreateAvatarSelectionEntry(128 * 0, 138, "data/sprites/avatars/male_1_128.png", avatarButton, avatarContainer, avatarButtonStyle);
+            male1.OnClick += () => avatarIndex = 5;
+            var male2 = UIFactory.CreateAvatarSelectionEntry(128 * 1 + 10, 138, "data/sprites/avatars/male_2_128.png", avatarButton, avatarContainer, avatarButtonStyle);
+            male2.OnClick += () => avatarIndex = 6;
+            var male3 = UIFactory.CreateAvatarSelectionEntry(128 * 2 + 10, 138, "data/sprites/avatars/male_3_128.png", avatarButton, avatarContainer, avatarButtonStyle);
+            male3.OnClick += () => avatarIndex = 7;
+            var male4 = UIFactory.CreateAvatarSelectionEntry(128 * 3 + 10, 138, "data/sprites/avatars/male_4_128.png", avatarButton, avatarContainer, avatarButtonStyle);
+            male4.OnClick += () => avatarIndex = 8;
+            var male5 = UIFactory.CreateAvatarSelectionEntry(128 * 4 + 10, 138, "data/sprites/avatars/male_5_128.png", avatarButton, avatarContainer, avatarButtonStyle);
+            male5.OnClick += () => avatarIndex = 9;
+            avatarContainer.AddChild(female1);
+            avatarContainer.AddChild(female2);
+            avatarContainer.AddChild(female3);
+            avatarContainer.AddChild(female4);
+            avatarContainer.AddChild(female5);
+            avatarContainer.AddChild(male1);
+            avatarContainer.AddChild(male2);
+            avatarContainer.AddChild(male3);
+            avatarContainer.AddChild(male4);
+            avatarContainer.AddChild(male5);
+            
+            body.AddChild(new Checkbox(0, 270+138, Globals.WIDTH - 40, 70, "Would you like to share anonymous\nusage statistics to help us\nimprove the user experience?", onValueChanged: (oldValue, newValue) => {
                 consent = newValue;
             }) {IsChecked = true});
             body.AddChild(new Button(20, Globals.HEIGHT - 40 - 190 - 30, Globals.WIDTH - 80, 40, "Submit", onClick: () => {
