cd634146561186f9875f289a9bd126e85d6cf7c9
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2020-05-13 18:47:59 +0200

    Add final art on map scene

--- client/Game/Scenes/MapScene.cs
+++ client/Game/Scenes/MapScene.cs
@@ -1,25 +1,47 @@
 using System.Drawing;
 using game.ui;
 using game.utils;
+using GXPEngine;
+using Button = game.ui.Button;
+using Image = game.ui.Image;
 
 namespace game {
     public class MapScene : Scene {
+
+        private Image coffeeTooltip;
+        private Image felliniTooltip;
+        private Image rocksTooltip;
+        
         public MapScene() {
             SceneName = "Map";
         }
         
         public override void Load() {
-            var primaryTitleStyle = new LabelStyle(Color.White, 32f, FontLoader.CenterCenterAlignment);
-            Root.AddChild(new Label(40, 20, Globals.WIDTH - 80, 40, "CHOOSE A LOCATION", primaryTitleStyle));
-            Root.AddChild(new Button(40, 80, Globals.WIDTH - 80f, 40, "Fellini", onClick: () => {
+            Root.AddChild(new Image(0, 0, Globals.WIDTH, Globals.HEIGHT, new Sprite("data/sprites/map/map.jpg")));
+            Root.AddChild(new Button(887, 0, 200, 320, "Fellini", ButtonStyle.Transparent, () => {
                 NetworkManager.Instance.JoinLocation("Fellini");
+            }, () => {
+                felliniTooltip.x = 612f;
+            }, () => {
+                felliniTooltip.x = -100000f;
             }));
-            Root.AddChild(new Button(40, 140, Globals.WIDTH - 80f, 40, "Rock's", onClick: () => {
+            Root.AddChild(new Button(1075, 572, 200, 300, "Rock's", ButtonStyle.Transparent, () => {
                 NetworkManager.Instance.JoinLocation("Rock's");
+            }, () => {
+                rocksTooltip.x = 800f;
+            }, () => {
+                rocksTooltip.x = -100000f;
             }));
-            Root.AddChild(new Button(40, 200, Globals.WIDTH - 80f, 40, "Coffee Fellows", onClick: () => {
+            Root.AddChild(new Button(423, 88, 200, 320, "Coffee Fellows", ButtonStyle.Transparent, () => {
                 NetworkManager.Instance.JoinLocation("Coffee Fellows");
+            }, () => {
+                coffeeTooltip.x = 145f;
+            }, () => {
+                coffeeTooltip.x = -100000f;
             }));
+            Root.AddChild(coffeeTooltip = new Image(145, 445, 750, 150, new Sprite("data/sprites/map/coffee_fellows_slogan.jpg")) {x = -100000f});
+            Root.AddChild(felliniTooltip = new Image(612, 375, 750, 150, new Sprite("data/sprites/map/fellini_slogan.jpg")) {x = -100000f});
+            Root.AddChild(rocksTooltip = new Image(800, 400, 750, 150, new Sprite("data/sprites/map/rocks_slogan.jpg")) {x = -100000f});
             IsLoaded = true;
         }
     }
