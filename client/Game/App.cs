3aeeb71c54a3844d9c20ece6b7bc8f39f42269ce
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2020-05-15 13:50:19 +0200

    Change loading screen text to something more suitable

--- client/Game/App.cs
+++ client/Game/App.cs
@@ -14,7 +14,6 @@ namespace game {
         public App() : base(Globals.WIDTH, Globals.HEIGHT, Globals.FULLSCREEN, Globals.VSYNC, pPixelArt: Globals.PIXEL_ART, windowTitle: Globals.WINDOW_TITLE) {
             // Debug.LogWarning("TODO: Enable logging on server");
             // Debug.LogWarning("TODO: Add website to server (using express)");
-            // Debug.LogWarning("TODO: Change the loading assets text to something like `Go grab a beer, get comfortable and bla bla bla...`");
             // Debug.LogWarning("TODO: Change scenes to keep static assets loaded and just hidden off-screen instead of reloading assets every time there is a scene change");
             // Environment.Exit(0);
             // targetFps = 60;
